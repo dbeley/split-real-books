1b77fdacbf58a4421802d4957034983a7871e3a6
dbeley <6568955+dbeley@users.noreply.github.com>
2025-06-07 16:51:47 +0200

    rsync: add --ignore-existing

--- transfert.sh
+++ transfert.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -azvhP --no-t --stats --inplace --zc=zstd --zl=3 output_songs/ ~/Nextcloud/20-29_Médias/20_Partitions/20.05_Real-Books-Individual-Songs/
-rsync -azvhP --no-t --stats --inplace --zc=zstd --zl=3 christmas/ ~/Nextcloud/20-29_Médias/20_Partitions/20.06_Christmas-Individual-Songs/
+rsync -azvhP --no-t --stats --inplace --zc=zstd --ignore-existing --zl=3 output_songs/ ~/Nextcloud/20-29_Médias/20_Partitions/20.05_Real-Books-Individual-Songs/
+rsync -azvhP --no-t --stats --inplace --zc=zstd --ignore-existing --zl=3 christmas/ ~/Nextcloud/20-29_Médias/20_Partitions/20.06_Christmas-Individual-Songs/
