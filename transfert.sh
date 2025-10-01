3407b43134a80a00e750c3ad5053e4855d799b56
dbeley <6568955+dbeley@users.noreply.github.com>
2025-10-02 01:22:46 +0200

    transfert.sh: add combined pdf

--- transfert.sh
+++ transfert.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rsync -azvhP --stats --no-t --inplace --zc=zstd --update --delete-after --zl=3 --checksum output_songs/ ~/Nextcloud/20-29_Médias/20_Partitions/20.05_Real-Books-Individual-Songs/
 rsync -azvhP --stats --no-t --inplace --zc=zstd --update --delete-after --zl=3 --checksum christmas/ ~/Nextcloud/20-29_Médias/20_Partitions/20.06_Christmas-Individual-Songs/
+rsync -azvhP --stats --no-t --inplace --zc=zstd --update --delete-after --zl=3 --checksum output_songs_combined.pdf ~/Nextcloud/20-29_Médias/20_Partitions/
